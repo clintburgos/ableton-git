5f07be2e0739dace3af625ab92d8eabee327d828
Clinton Burgos <clintburgos@users.noreply.github.com>
2019-03-03 12:31:59 -0500

    repo setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate fs_extra;
 extern crate regex;
 
 use regex::Regex;
@@ -5,6 +6,10 @@ use std::env;
 use std::process;
 use std::process::Command;
 use std::string::String;
+use fs_extra::dir;
+use std::fs::OpenOptions;
+use std::fs;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,13 +26,16 @@ fn main() {
         return;
     }
 
-    let repo_directory = get_repo_directory(args);
+    let repo_directory = get_repo_directory(&args);
 
-    println!("{}", repo_directory);
+    if &args[1] == "init" {
+        copy_repo_files(&repo_directory);
+    }
 
+    append_config(&repo_directory);
 }
 
-fn get_repo_directory(args: Vec<String>) -> String {
+fn get_repo_directory(args: &Vec<String>) -> String {
     if &args[1] == "init" {
         return String::from(".");
     }
@@ -39,12 +47,31 @@ fn get_repo_directory(args: Vec<String>) -> String {
 }
 
 fn derive_project_name_from_repository_path(repository_path: &String) -> String {
-    let project_name_regex = Regex::new("(?:.*/)(?P<project_name>.*)(?:.git)").unwrap();
+    let project_name_regex = Regex::new("(?:.*/)(?P<project_name>.*)(?:.git)")
+        .expect("Could not construct project name regex");
+
     return match project_name_regex.captures(repository_path) {
-        Some(captures) => String::from(captures.name("project_name").unwrap().as_str()),
+        Some(captures) => String::from(captures.name("project_name")
+            .expect("Could not capture project name in repository path").as_str()),
         None => {
             println!("Invalid repository path.");
             process::exit(1);
         }
     };
+}
+
+fn copy_repo_files(repo_directory: &String) {
+    dir::copy("repo_files", repo_directory, &dir::CopyOptions::new());
+}
+
+fn append_config(repo_directory: &String) {
+    let mut repository_config_file = OpenOptions::new()
+        .append(true)
+        .open(format!("{}/.git/config", repo_directory))
+        .expect("Could not open .git/config for appending");
+
+    let config_to_append = fs::read_to_string("src/templates/config.txt")
+        .expect("Could not read config template");
+
+    repository_config_file.write_all(config_to_append.as_bytes());
 }
---
+++ src/repo_files/README.md
@@ -0,0 +1,6 @@
+# Ableton Project
+
+Created with \`ableton-git\`.
+
+Please use \`ableton-git\` to clone this project :)
+<https://github.com/clintburgos/ableton-git>
