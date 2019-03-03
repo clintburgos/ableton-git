749412560d2f3f85d438e78c2e4f134a7d612525
Clinton Burgos <clintburgos@users.noreply.github.com>
2019-03-03 12:38:06 -0500

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 extern crate fs_extra;
 extern crate regex;
 
+use fs_extra::dir;
 use regex::Regex;
 use std::env;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 use std::process;
 use std::process::Command;
 use std::string::String;
-use fs_extra::dir;
-use std::fs::OpenOptions;
-use std::fs;
-use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Pass args to git first.
     // For the most part, Ableton projects can be version controlled with unwrapped git.
-//    Command::new("git")
-//            .args(&args[1..])
-//            .spawn()
-//            .expect("Error occurred calling git. Is git installed?");
+    Command::new("git")
+        .args(&args[1..])
+        .spawn()
+        .expect("Error occurred calling git. Is git installed?");
 
     // We're only wrapping init and clone to ensure the repo is set up correctly.
     if !(&args[1] == "init" || &args[1] == "clone") {
@@ -42,7 +42,7 @@ fn get_repo_directory(args: &Vec<String>) -> String {
 
     match args.get(3) {
         None => derive_project_name_from_repository_path(&args[2]),
-        _ => args[3].clone()
+        _ => args[3].clone(),
     }
 }
 
@@ -51,8 +51,12 @@ fn derive_project_name_from_repository_path(repository_path: &String) -> String 
         .expect("Could not construct project name regex");
 
     return match project_name_regex.captures(repository_path) {
-        Some(captures) => String::from(captures.name("project_name")
-            .expect("Could not capture project name in repository path").as_str()),
+        Some(captures) => String::from(
+            captures
+                .name("project_name")
+                .expect("Could not capture project name in repository path")
+                .as_str(),
+        ),
         None => {
             println!("Invalid repository path.");
             process::exit(1);
@@ -70,8 +74,8 @@ fn append_config(repo_directory: &String) {
         .open(format!("{}/.git/config", repo_directory))
         .expect("Could not open .git/config for appending");
 
-    let config_to_append = fs::read_to_string("src/templates/config.txt")
-        .expect("Could not read config template");
+    let config_to_append =
+        fs::read_to_string("src/templates/config.txt").expect("Could not read config template");
 
     repository_config_file.write_all(config_to_append.as_bytes());
 }
